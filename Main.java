6d681028b4adf3aefeac36ea2389e144d3b7b544
Helen-Gudkova <106183827+Helen-Gudkova@users.noreply.github.com>
2022-05-30 18:46:51 +0300

    1 задание

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        StringCoding coding = new StringCoding();
+        coding.setSourceString("aaaabbcaaaddde");
+        System.out.println(coding.getDestinationString());
+    }
+}
---
+++ StringCoding.java
@@ -0,0 +1,26 @@
+public class StringCoding {
+    private String sourceString;
+
+    public void setSourceString(String str) {
+        sourceString = str;
+    }
+
+    public String getDestinationString() {
+        int counter = 1;
+        String destinationString = "";
+
+        for(int i = 0; i < sourceString.length() - 1; i++) {
+            if (sourceString.charAt(i) != sourceString.charAt(i + 1)) {
+                destinationString += String.valueOf(counter) + sourceString.charAt(i);
+                counter = 0;
+            }
+            if (i == sourceString.length() - 2) {
+                destinationString += String.valueOf(++counter) + sourceString.charAt(i + 1);
+            }
+            counter++;
+        }
+        return destinationString;
+    }
+}
+
+
